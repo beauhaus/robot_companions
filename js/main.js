a8c6b1d80da7da621223be434d8fe2b1c08c723a
beauvelop <beau.html@gmail.com>
2017-12-28 00:28:05 -0500

    adds temporary modal & logic in main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-!(function () {
-    'use strict';
-    console.log("hello!");
-})();
+!(function() {
+    var modal = document.getElementById('simple-modal');
+    var modalBtn = document.getElementById('modal-btn');
+    var closeBtn = document.getElementById('close-btn');
+      
+      
+    function openModal() {
+      modal.style.display = "block";
+    }
+      function closeModal() {
+      console.log("close clicked");
+      modal.style.display = "none";
+    }
+    modalBtn.addEventListener('click', openModal);
+    closeBtn.addEventListener('click', closeModal);
+    
+    // listen for outside click
+      window.addEventListener('click', outsideClick);
+      function outsideClick(evt) {
+        if(evt.target === modal) {
+          modal.style.display = "none";
+        }
+      }
+    })()
