ab4663d8810c108e98820df625c33222b57bca7f
beauvelop <beau.html@gmail.com>
2018-01-07 17:16:29 -0500

    adds partnership desktop layout

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -4,37 +4,36 @@ import styled from 'styled-components';
 const StyledPartnership = styled.div`
 height: 86vh;
 color: black
-position: absolute;
 border: 2px solid navy;
 background: url(./img/buddies.jpg) no-repeat center center; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
-  // tablet query
-    @media screen and (max-width: 768px) {
-    height: 52vh;
-    }
-    @media screen and (max-width: 768px) {
-        height: 85vh;
-        & > .partnership-container {
-            margin-top: 8vh;
-        }
-    }
+//   // tablet query
+//     @media screen and (max-width: 768px) {
+//     height: 52vh;
+//     }
+//     @media screen and (max-width: 768px) {
+//         height: 85vh;
+//         & > .partnership-container {
+//             margin-top: 8vh;
+//         }
+//     }
 `
 const StyledPartnershipContainer = styled.article`
 margin: auto;
 color: #1d2535;
-padding: 18vh 0.5%;
-border: 1px solid maroon;
-height: 54vh;
-width: 70vw;
-position: relative;
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-grid-template-rows: 1fr;
-& > section#text-section {
-    position: relative;
+padding: 2vh 1vw;
+border: 2px solid maroon;
+height: 100%;
+width: 68vw;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+padding-top: 8%;
+& > section {
+    width: 50%;
     & h1 {
         font-size: 2rem;
         color: #1d2535;
@@ -50,45 +49,11 @@ grid-template-rows: 1fr;
         font-size: 0.7rem;
         color: black;
     }
-    
-} 
     & img {
-        position: absolute;
-        width: 12vw;
-        top: 0;
-        left: 50%;
-        margin-left: -6vw;
-        box-shadow: 4px 4px 6px 1px rgba(0,0,0,0.5);
+        box-shadow: 4px 4px 6px 3px rgba(0,0,0,0.8);
+        margin: 0 10vw;
     }
-    // tablet query
-    @media screen and (max-width: 768px) {
-        background: red;
-        position: relative;
-        & > section#text-section {
-            width: 50%;
-
-            position: absolute;
-            top: 2vh;
-            & p {
-                font-size: .6rem;
-            }
-            & > h1 {
-                display: block
-                position: absolute:
-                top: 2vh;
-                font-size: 1.2rem;
-                & > span {
-                    font-size: 1.2rem;
-                }
-            }
-        }
-        &  img {
-            width: 12vw;
-            top: 0;
-            right: 50%;
-            margin-right: 6vw;    
-        }
-    }
+} 
 
 `
 
